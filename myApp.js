9e1dcfe1b5b94aac2ebcaf47e2128956407a8733
jhordyess <65508346+jhordyess@users.noreply.github.com>
2024-01-04 20:39:17 +0000

    Challenge 4/12

--- myApp.js
+++ myApp.js
@@ -1,11 +1,14 @@
 let express = require("express");
 let app = express();
 
-const filePath = __dirname + "/views/index.html";
+const indexPath = __dirname + "/views/index.html";
+const assetsPath = __dirname + "/public";
 
 const hello = (req, res) => {
-  res.sendFile(filePath);
+  res.sendFile(indexPath);
 };
+
+app.use("/public", express.static(assetsPath));
 
 app.get("/", hello);
 
