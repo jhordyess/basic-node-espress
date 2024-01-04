7c39a7ef26164c5df4079fa2bdc559e79e5f0c1f
jhordyess <65508346+jhordyess@users.noreply.github.com>
2024-01-04 21:06:47 +0000

    Challenge 5/12

--- myApp.js
+++ myApp.js
@@ -4,12 +4,18 @@ let app = express();
 const indexPath = __dirname + "/views/index.html";
 const assetsPath = __dirname + "/public";
 
-const hello = (req, res) => {
+const index = (req, res) => {
   res.sendFile(indexPath);
+};
+
+const json = (req, res) => {
+  res.json({ message: "Hello json" });
 };
 
 app.use("/public", express.static(assetsPath));
 
-app.get("/", hello);
+app.get("/", index);
+
+app.get("/json", json);
 
 module.exports = app;
